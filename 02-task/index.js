381b061476a2377567bccb39dd1c12015aa4b64f
Manoryk Hanna <86349455+anna-manorik@users.noreply.github.com>
2025-03-04 00:15:59 +0200

    added calculation of incomes and costs

--- 02-task/index.js
+++ 02-task/index.js
@@ -1,18 +1,22 @@
 import { nanoid } from './node_modules/nanoid/nanoid.js';
 
 const addAmountBtn = document.getElementById("addAmountBtn");
-const amount = document.getElementById("amount");
+const amountInput = document.getElementById("amount");
 const amountType = document.getElementById("amountType");
 const amountDate = document.getElementById("amountDate");
 
 const incomesUl = document.getElementById("incomesList");
 const costsUl = document.getElementById("costsList");
 
+const totalIncomeSpan = document.getElementById("totalIncome");
+const totalCostsSpan = document.getElementById("totalCosts");
+
 document.addEventListener("DOMContentLoaded", () => {
     if(localStorage.getItem('amountList')) {
         const amountList = JSON.parse(localStorage.getItem('amountList'));
 
         amountList.forEach(amountRecord => createRecord(amountRecord));
+        totalIncomeSpan.innerHTML = localStorage.getItem('totalIncome')
     }
 });
 
@@ -22,36 +26,59 @@ addAmountBtn.addEventListener('click', (e) => {
     let amountList = JSON.parse(localStorage.getItem('amountList')) || [];
     const newAmount = {
         id: nanoid(),
-        amount: amount.value,
+        amount: amountInput.value,
         amountType: amountType.value,
         amountDate: amountDate.value
     }
     
-    if(amount.value === '') {
-        
+    if(amountInput.value === '') {
         toastr.error('Please, fill amount!');
         return
     }
 
     createRecord(newAmount);
+    calcTotal(amountInput.value, amountType.value);
 
     localStorage.setItem("amountList", JSON.stringify([newAmount, ...amountList]));
     toastr.success('New record was created successfully!')
-    amount.value = ''
+    amountInput.value = ''
 })
 
 function createRecord ({ id, amount, amountType, amountDate }) {
     const li = document.createElement("li");
     li.innerHTML = `
-        <span class="description">${amount}</span>
+        <span class="amount">${amount}</span>
         <span>${amountType}</span>
         <button class="delete-btn" id=${id}>❌</button>
     `;
 
     if(amountType === 'incomes') {
-        incomesUl.appendChild(li)
+        incomesUl.appendChild(li);
     } else if (amountType === 'costs') {
-        costsUl.appendChild(li)
+        costsUl.appendChild(li);
     }
 
+}
+
+function calcTotal(amount, amountType) {
+    if (amountType === 'incomes') {
+        if(!JSON.parse(localStorage.getItem('totalIncome'))) {
+            localStorage.setItem("totalIncome", amount);
+            totalIncome.innerHTML = amount
+        } else {
+            const totalIncome = Number(localStorage.getItem('totalIncome')) + Number(amount);
+            localStorage.setItem("totalIncome", totalIncome);
+            totalIncomeSpan.innerHTML = totalIncome
+        }
+    } else if (amountType === 'costs') {
+        if(!JSON.parse(localStorage.getItem('totalCosts'))) {
+            localStorage.setItem("totalCosts", amount);
+            totalCostsSpan.innerHTML = amount
+        } else {
+            const totalCosts = Number(localStorage.getItem('totalCosts')) + Number(amount);
+            localStorage.setItem("totalCosts", totalCosts);
+            totalCostsSpan.innerHTML = totalCosts
+        }
+    }
+    
 }
