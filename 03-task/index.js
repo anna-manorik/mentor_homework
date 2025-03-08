70adb27dc9c20a4dd677afbd64f8c0474363d8fa
Manoryk Hanna <86349455+anna-manorik@users.noreply.github.com>
2025-03-09 00:07:18 +0200

    added auto sliding

--- 03-task/index.js
+++ 03-task/index.js
@@ -1,45 +1,52 @@
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
+const autoBtn = document.getElementById("auto");
 const currentImg = document.querySelector('.active')
 
 const listItems = document.querySelectorAll(".slider-img");
 const images = Array.from(listItems).map((img, index) => img.id = index + 1);
 let currentImgId = Number(currentImg.getAttribute('id'))
 
-nextBtn.addEventListener('click', () => {
+function slideNext () {
     const currentImg = document.querySelector('.active')
-
-    if(currentImgId < listItems.length) {
-        currentImg.classList.remove('active')
-        document.getElementById(currentImgId + 1).classList.add('active')
-        currentImgId += 1;
-        console.log("!!!", currentImgId)
-    }
 
     if(currentImgId === listItems.length) {
         currentImg.classList.remove('active')
         document.getElementById(1).classList.add('active')
         currentImgId = 1;
+        return
     }
 
+    if(currentImgId < listItems.length) {
+        currentImg.classList.remove('active')
+        document.getElementById(currentImgId + 1).classList.add('active')
+        currentImgId += 1;
+    }
+}
+
+nextBtn.addEventListener('click', () => {
+    slideNext()
 })
 
 prevBtn.addEventListener('click', () => {
     const currentImg = document.querySelector('.active')
 
     if(currentImgId === 1) {
-        console.log("1111", currentImgId)
         currentImg.classList.remove('active')
         document.getElementById(listItems.length).classList.add('active')
         currentImgId = listItems.length;
+        return
     }
 
     if(currentImgId <= listItems.length) {
         currentImg.classList.remove('active')
         document.getElementById(currentImgId - 1).classList.add('active')
         currentImgId -= 1;
-        console.log("!!!", currentImgId)
     }
+})
 
-   
+autoBtn.addEventListener ('click', () => {
+    setInterval(() => {
+         slideNext()
+    }, 2000);
 })
